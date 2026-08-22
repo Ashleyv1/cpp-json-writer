f67e3611c239044d619f181256f2480a017504c2
Doppler2u <adarshthapa95555@gmail.com.com>
2026-08-22 19:33:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cpp-json-writer
+Compact properties parser mapping JSON structures output.
+Compile: `g++ src/writer.cpp -o writer`
---
+++ src/writer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "JSON serializer mapping properties keys..." << std::endl;
+    std::cout << "{\"status\": \"OK\", \"code\": 200}" << std::endl;
+    return 0;
+}
